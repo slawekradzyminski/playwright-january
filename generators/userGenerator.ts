a54d6f018a23cdd731fa201aea4c5597955f2ed7
slawekradzyminski <slawomir.radzyminski@gmail.com>
2025-01-14 08:57:42 +0100

    Update project files

--- generators/userGenerator.ts
+++ generators/userGenerator.ts
@@ -1,13 +1,11 @@
-import { faker } from '@faker-js/faker';
-import type { User, UserRole } from '../types/User';
+import { faker } from "@faker-js/faker";
+import type { User, UserRole } from "../types/User";
 
 const MAX_RETRIES = 10;
 
-const generateValidValue = <T>(
-  generator: () => T,
-  validator: (value: T) => boolean,
-  errorMessage: string
-): T => {
+const generateValidValue = <T>(generator: () => T): T => {
+  const validator = (value: T) => typeof value === "string" && value.length >= 4;
+
   let retryCount = 0;
   let value = generator();
 
@@ -17,45 +15,19 @@ const generateValidValue = <T>(
   }
 
   if (!validator(value)) {
-    throw new Error(errorMessage);
+    throw new Error(`Could not generate valid value after maximum retries`);
   }
-
   return value;
 };
 
 export const getRandomUser = (): User => {
-  const firstName = generateValidValue(
-    () => faker.person.firstName(),
-    (value) => value.length >= 4,
-    'Could not generate valid firstName after maximum retries'
-  );
-
-  const lastName = generateValidValue(
-    () => faker.person.lastName(),
-    (value) => value.length >= 4,
-    'Could not generate valid lastName after maximum retries'
-  );
-
-  const username = generateValidValue(
-    () => faker.internet.username(),
-    (value) => value.length >= 4,
-    'Could not generate valid username after maximum retries'
-  );
-
-  const password = generateValidValue(
-    () => faker.internet.password(),
-    (value) => value.length >= 4,
-    'Could not generate valid password after maximum retries'
-  );
-
-  const roles: UserRole[] = ['ROLE_ADMIN', 'ROLE_CLIENT'];
-
   return {
-    username,
-    email: faker.internet.email({ firstName, lastName }),
-    password,
-    roles,
-    firstName,
-    lastName,
+    username: generateValidValue(faker.internet.username),
+    email: faker.internet.email(),
+    password: generateValidValue(faker.internet.password),
+    roles: ["ROLE_ADMIN", "ROLE_CLIENT"],
+    firstName: generateValidValue(faker.person.firstName),
+    lastName: generateValidValue(faker.person.lastName),
   };
 };
+
